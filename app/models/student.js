6ccd71e921e66986ced3997864bc59a225035061
Ipaye Alameen <Akinkunmi007@gmail.com>
2017-10-14 22:38:18 +0100

    update student schema

--- app/models/student.js
+++ app/models/student.js
@@ -2,22 +2,41 @@ const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 
 const studentSchema = new schema({
-  firstName : {
-    type : String,
-  },
-  lastName : {
-    type : String,
-  },
-  email : {
-    type : String,
-  },
-  address : {
-    type : String,
+  firstName: {
+    type: String,
     required: true
   },
-  phoneNumber : {
-    type : String,
-  }  
+  lastName: {
+    type: String,
+    required: true
+  },
+  gender: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  phoneNumber: {
+    type: String,
+  },
+  fatherName: {
+    type: String,
+  },
+  motherName: {
+    type: String,
+  },
+  parentPhoneNumber: {
+    type: String,
+  },
+  courses : [{
+    type : schema.Types.ObjectId,
+    ref : 'course'
+  }]
 });
 
 
