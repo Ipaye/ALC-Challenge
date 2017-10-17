ba7229d3e06ca83d9e1f1eb6dca90a4bbf5c6072
Ipaye Alameen <Akinkunmi007@gmail.com>
2017-10-17 10:21:02 +0100

    add validation for student Id

--- app/helpers/validator.js
+++ app/helpers/validator.js
@@ -1,12 +1,26 @@
 const Joi = require("joi");
 
 module.exports = {
-  idSchema: {
-   return Joi.object().keys({
-      username: Joi.string().alphanum().min(3).max(30).required(),
-      password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/),
-      birthyear: Joi.number().integer().min(1900).max(2013),
-      email: Joi.string().email()
+  validateParam : (schema, name)=>{
+    return (req, res, next)=>{
+      console.log('req-param', req.params);
+      const result = Joi.validate({param : req['params'][name]}, schema);
+      if(result.error){
+        return res.status(400).json(result.error);
+      }else{
+        if(!req.value) {
+            req.value = {}
+        }if(!req.value['params'])
+        req.value['params']={};
+
+        req.value['params'][name] = result.value.param;
+        next();
+      }
+    }
+  },
+  schemas: {
+    idSchema : Joi.object().keys({
+      param : Joi.string().regex(/^[0-9a-fA-F]{24}$/).required()
     })
   }
 
