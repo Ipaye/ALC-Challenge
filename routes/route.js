fe14345c19431764db2dfb6979d6dffa25d46cbc
Ipaye Alameen <Akinkunmi007@gmail.com>
2017-10-17 10:21:25 +0100

    implement validation for student ID

--- routes/route.js
+++ routes/route.js
@@ -2,17 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 const studentController = require('../app/controllers/studentController');
+const { validateParam, schemas } = require('../app/helpers/validator');
+
 
 router.route('/')
   .get(studentController.getAllStudents)
   .post(studentController.createStudent);
 
 router.route('/:studentId')
-  .get(studentController.getAStudent)
-  .patch(studentController.updateAStudent)
-  .delete(studentController.deleteAStudent);
+  .get(validateParam(schemas.idSchema, 'studentId') ,studentController.getAStudent)
+  .patch(validateParam(schemas.idSchema, 'studentId') ,studentController.updateAStudent)
+  .delete(validateParam(schemas.idSchema, 'studentId'), studentController.deleteAStudent);
 
   router.route('/:studentId/course')
-  .get(studentController.getAStudentCourse)
-  .post(studentController.makeAStudentCourse);
+  .get(validateParam(schemas.idSchema, 'studentId'),studentController.getAStudentCourse)
+  .post(validateParam(schemas.idSchema, 'studentId'), studentController.makeAStudentCourse);
 module.exports = router;
