c5436110e1f84608864627b3bd19bb8b4bb66e48
Ipaye Alameen <Akinkunmi007@gmail.com>
2017-11-02 00:25:09 +0100

    refactored edit routes

--- routes/student.js
+++ routes/student.js
@@ -41,6 +41,34 @@ router.get('/view/:id', function (req, res, next) {
   });
 });
 
+router.post('/edit/:id', function (req, res, next) {
+  const studentId = req.params.id;
+  const studentDetail = req.body;
+  const options = {
+    url: 'https://alc-student-resource.herokuapp.com/students/' + studentId,
+    method: 'PATCH',
+    headers: {
+      'Accept': 'application/json',
+    },
+    json: true,
+    body: studentDetail
+  };
+
+  request(options, function (error, response, body) {
+    console.log(response);
+    if (!error && response.statusCode == 200) {
+      let success = response.message;
+      req.flash('success_msg', 'Student Record Updated Successfully');
+      res.redirect(`/student`);
+    }
+    if (error && response.statusCode == 400) {
+      req.flash('error_msg', 'Student Record Failed to Update Record 😦, check your inputs.');
+      res.render('editStudent');
+    }
+  });
+})
+
+
 router.get('/edit/:id', function (req, res, next) {
   const studentId = req.params.id;
   console.log(studentId);
@@ -95,6 +123,10 @@ router.post('/add', function (req, res, next) {
       let success = response.message;
       req.flash('success_msg', 'Student Record Added Successfully');
       res.redirect('/student');
+    }
+    if (error && response.statusCode == 400) {
+      req.flash('error_msg', 'Student Record Failed to Add 😦, check your inputs.');
+      res.render('addStudent');
     }
   });
 })
